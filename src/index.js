48e517aed4b214ab50f5660e3c2f1faaad6cb1e8
Jeff Shuo <jeffshuo@gmail.com>
2022-05-23 00:17:32 +0800

    fio transaction ok

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles.css'
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import { Button, Modal, Form } from 'react-bootstrap'
+import { Button, Modal, Form, Alert } from 'react-bootstrap'
 import AddForm from './AddForm'
 import * as crypto from 'crypto'
 const bs58 = require('bs58')
@@ -12,6 +12,15 @@ import Web3 from 'web3'
 import { getAddressBalances } from 'eth-balance-checker/lib/web3'
 const provider = require('eth-provider')
 const { FIOSDK } = require('@fioprotocol/fiosdk')
+
+const generateTestingFioDomain = () => {
+  return `testing-domain-${Math.floor(Math.random() * 1000)}`
+}
+const payerfioAddress = 'jshuo@fiotestnet'
+const payeefioAddress = 'secux@fiotestnet'
+
+let privateKey = '5KDEgao6gCx8KvCgJVn5Nm6W4X9EkS8Uap4QydX2yezZrvkT9E6',
+  publicKey = 'FIO5QvSU16Z6h57WhfqBJ1z1TrLUvUW5etmeHygfGhDFz6mQiJdcE'
 
 const fetchJson = async (uri, opts = {}) => {
   return fetch(uri, opts)
@@ -41,16 +50,26 @@ function App() {
     const FIO_PUBLIC_PREFIX = 'FIO'
     let pubBuf = await transport.getPublickey(FIO_ACCOUNT_PATH, 0)
     console.log(pubBuf.toString('hex'))
+    const fio_address = await FIOSDK.getPublicKey(transport, FIO_ACCOUNT_PATH)
     let checksum = crypto.createHash('rmd160').update(pubBuf).digest('hex').slice(0, 8)
     pubBuf = Buffer.concat([pubBuf, Buffer.from(checksum, 'hex')])
     const FIOPubkeyAddress = FIO_PUBLIC_PREFIX.concat(bs58.encode(pubBuf))
-    const fio = new FIOSDK('', '', 'http://testnet.fioprotocol.io/v1/', fetchJson, undefined, '')
+    const fioSdk = new FIOSDK(privateKey, publicKey, 'http://testnet.fioprotocol.io/v1/', fetchJson, transport, '')
+    // const defaultFee = 800 * fioSdk.FIOSDK.SUFUnit
     // const FIOAddress = await fio.genericAction('getFioAddresses', {fioPublicKey: pk.toString('hex')})
-    const FIOAddress = await fio.getFioAddresses(FIOPubkeyAddress)
+    const FIOAddress = await fioSdk.getFioAddresses(FIOPubkeyAddress)
+    const { public_address: payeePublicKey } = await fioSdk.getPublicAddress(payeefioAddress, "FIO", "FIO")
     console.log(FIOAddress.fio_addresses[0].fio_address)
-    const fio_address = await FIOSDK.getPublicKey(transport, FIO_ACCOUNT_PATH)
-    console.log(fio_address.publicKey.toString('hex'))
+    const { fee } = await fioSdk.getFee('transfer_tokens_pub_key')
+    const transferAmount = 168000000 // 1 FIO
 
+    const result = await fioSdk.pushTransaction('fio.token', 'trnsfiopubky', {
+      payee_public_key: payeePublicKey,
+      amount: transferAmount,
+      max_fee: fee,
+      tpid: payerfioAddress
+    })
+    console.log(result)
     const address = await SecuxETH.getAddress(transport, SECUX_ETH_ACCOUNT_PATH)
     // const web3 = new Web3(provider("infura", { infuraId: INFURA_ID }));
     const web3 = new Web3(provider(`wss://rinkeby.infura.io/ws/v3/${INFURA_ID}`))
